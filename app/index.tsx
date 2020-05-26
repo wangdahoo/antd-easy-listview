c90d60df5420e45c30fd69482e2c2f4939b26c48
wangdahoo <157195705@qq.com>
2020-05-27 01:55:26 +0800

    v0.1.16

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import './index.less'
 import 'antd/dist/antd.css'
+import './index.less'
 import App from './App'
 import { ConfigProvider } from 'antd'
 import zhCN from 'antd/es/locale/zh_CN'
